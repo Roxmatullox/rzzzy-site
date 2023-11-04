0439cac836e48a84a3cd44ad4d11c8118ae9a235
Roxmatullox <roxmatulloxprgrmr@gmail.com>
2023-11-04 20:14:46 +0500

    rox

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,17 @@
+import { useEffect , useState } from "react";
 import { useLocation } from "react-router-dom"
 
-import { message } from "antd";
-import { useEffect } from "react";
+import { message , Spin } from "antd";
 
 import "./HomePage.scss"
 
 const HomePage = () => {
+
+  const [loading , setLoading] = useState(true)
+
+  window.addEventListener("load", () => {
+    setLoading(false)
+  });
 
   useEffect(()=>{
     message.success("Rzzzy saytiga hush kelibsiz !")
@@ -16,28 +22,30 @@ const HomePage = () => {
   const {pathname : page} = location
     
   return (
-    <section id="home">
-      <div className="home">
-      </div>
-      <div className="three-pages">
-        <div className="container">
-            {
-              page === "/" ? <div className="home-page" >
-                <h1>Home</h1>
-              </div> : <></>
-            }
-            {
-              page === "/login" ? <div className="login-page" >
-                <h1>Login</h1>
-              </div> : <></>
-            }{
-              page === "/register" ? <div className="register-page" >
-                <h1>Register</h1>
-              </div> : <></>
-            }
+    <Spin spinning={loading} size="large">
+      <section id="home">
+        <div className="home">
         </div>
-      </div>
-    </section>
+        <div className="three-pages">
+          <div className="container">
+              {
+                page === "/" ? <div className="home-page" >
+                  <h1>Home</h1>
+                </div> : <></>
+              }
+              {
+                page === "/login" ? <div className="login-page" >
+                  <h1>Login</h1>
+                </div> : <></>
+              }{
+                page === "/register" ? <div className="register-page" >
+                  <h1>Register</h1>
+                </div> : <></>
+              }
+          </div>
+        </div>
+      </section>
+    </Spin>
   )
 }
 
