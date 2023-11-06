b57bc09d63c6f141c09c802cc5f67aaaf22703cf
Roxmatullox <roxmatulloxprgrmr@gmail.com>
2023-11-06 11:21:06 +0500

    rox

--- src/pages/AdminDashboardPage/AdminDashboardPage.tsx
+++ src/pages/AdminDashboardPage/AdminDashboardPage.tsx
@@ -1,6 +1,78 @@
+import {useEffect} from "react"
+
+import { Progress } from "antd"
+import useEducation from "../../zustand/education"
+import useSkills from "../../zustand/skills"
+
+import "./AdminDashboard.scss"
+
 const AdminDashboardPage = () => {
+
+  const {total : educationTotal , getData : getEducation , loading : educationLoadnig} = useEducation()
+
+  const {total : skillsTotal , getData : getSkills , loading : skillsLoading} = useSkills()  
+
+  useEffect(()=>{
+    getSkills()
+    getEducation()
+  } , [getSkills , getEducation])
+  
+
   return (
-    <div>AdminDashboardPage</div>
+    <div className="dashboard-admin">
+      <div className="container">
+        <div className="dashboard-cards">
+          <div className="dashboard-card">
+            <h3>Skills : ({skillsLoading ? <div className="loader"></div> : skillsTotal})</h3>
+            <hr />
+            <h4>From 200 :</h4>
+            <Progress type="dashboard" percent={skillsTotal / 2} />
+          </div>
+          <div className="dashboard-card">
+            <h3>Educations : ({educationLoadnig ? <div className="loader"></div> : educationTotal})</h3>
+            <hr />
+            <h4>From 200 :</h4>
+            <Progress type="dashboard" percent={educationTotal / 2} />
+          </div>
+          <div className="dashboard-card">
+            <h3>Soon : ()</h3>
+            <hr />
+            <h4>From 200 :</h4>
+            <Progress type="dashboard" percent={0} />
+          </div>
+          <div className="dashboard-card">
+            <h3>Soon : ()</h3>
+            <hr />
+            <h4>From 200 :</h4>
+            <Progress type="dashboard" percent={0} />
+          </div>
+          <div className="dashboard-card">
+            <h3>Soon : ()</h3>
+            <hr />
+            <h4>From 200 :</h4>
+            <Progress type="dashboard" percent={0} />
+          </div>
+          <div className="dashboard-card">
+            <h3>Soon : ()</h3>
+            <hr />
+            <h4>From 200 :</h4>
+            <Progress type="dashboard" percent={0} />
+          </div>
+          <div className="dashboard-card">
+            <h3>Soon : ()</h3>
+            <hr />
+            <h4>From 200 :</h4>
+            <Progress type="dashboard" percent={0} />
+          </div>
+          <div className="dashboard-card">
+            <h3>Soon : ()</h3>
+            <hr />
+            <h4>From 200 :</h4>
+            <Progress type="dashboard" percent={0} />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
